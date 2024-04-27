cd3ab3c1de892134ff332dda9bda49fa713ccc57
Marco Antonio Rizzi Meneguetti <marco.meneguetti@sou.inteli.edu.br>
2024-04-27 15:49:34 -0300

    update pub

--- semana-02/meu_workspace/src/ponderada_s2/ponderada_s2/ponderada_pub.py
+++ semana-02/meu_workspace/src/ponderada_s2/ponderada_s2/ponderada_pub.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+import math
 
 class TurtleController(Node):
     def __init__(self):
@@ -10,31 +11,38 @@ class TurtleController(Node):
             topic='/turtle1/cmd_vel',
             qos_profile=10
         )
-        timer_period = 0.5
-        self.timer = self.create_timer(
-            timer_period_sec=timer_period,
-            callback=self.timer_callback
-        )
-        self.count = 0 
-    
-    def timer_callback(self):
+        self.timer_period = 1.0
+        self.side_count = 0
+        self.max_sides = 4
+
+    def straight(self):
         msg = Twist()
-        if self.count < 10:
-            msg.linear.x = 1.0
-            msg.angular.z = 1.0
-        elif self.count > 10    :
-            msg.linear.x = 2.0
-            msg.angular.z = 2.0
-        else:
-            msg.linear.x = 0.0
-            msg.angular.z = 0.0
+        msg.linear.x = 2.0
+        msg.angular.z = 0.0
         self.publisher.publish(msg)
-        self.get_logger().info(f"Publicando velocidades para a tartaruga: angular {msg.angular.z} e linear {msg.linear.x}")
-        self.count += 1  #
+        self.get_logger().info(f"Andando com a tartaruga: angular {msg.angular.z} e linear {msg.linear.x}")
+
+    def curve(self):
+        msg = Twist()
+        msg.linear.x = 0.0
+        msg.angular.z = math.pi/3
+        self.publisher.publish(msg)
+        self.get_logger().info(f"Girando a tartaruga em 60 graus positivos: angular {msg.angular.z} e linear {msg.linear.x}")
+
+    def timer_callback(self):
+        if self.side_count < self.max_sides * 3:  # Multiplicando por 2 para garantir que a tartaruga desenhe todos os lados
+            if self.side_count % 2 == 0:
+                self.straight()
+            else:
+                self.curve()
+            self.side_count += 1
+        else:
+            self.timer.cancel()  # Cancela o temporizador após desenhar todos os lados do polígono
 
 def main(args=None):
     rclpy.init(args=args)
     tc = TurtleController()
+    tc.timer = tc.create_timer(tc.timer_period, tc.timer_callback)
     rclpy.spin(tc)
     tc.destroy_node()
     rclpy.shutdown()
